14ae4e5a02640fb59e277d496186a3170f7f8690
Fernando de Barros <108085545+fernandobarrosd@users.noreply.github.com>
2022-06-24 18:09:20 -0300

    relogio-js update 2

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-const horas = document.querySelector("#horas");
-const minutos = document.querySelector("#minutos");
-const segundos = document.querySelector("#segundos");
-
-
-
-
-window.addEventListener("load", () => {
-    setInterval(() => {
-       const date = new Date();
-       const dtHoras = date.getHours();
-       const dtMinutos = date.getMinutes();
-       const dtSegundos = date.getSeconds();
-       setRelogioValue(horas, dtHoras);
-       setRelogioValue(minutos, dtMinutos);
-       setRelogioValue(segundos, dtSegundos);
-    })
-})
-
 function setRelogioValue(element, value) {
     if (value < 10) {
         element.textContent = "0" + value;
@@ -25,3 +6,32 @@ function setRelogioValue(element, value) {
         element.textContent = value;
     }
 }
+
+function createClock() {
+    const date = createDate();
+    const clockValues = getClockValues();
+    setRelogioValue(clockValues[0], date[0]);
+    setRelogioValue(clockValues[1], date[1]);
+    setRelogioValue(clockValues[2], date[2]);
+}
+
+function createDate() {
+    const date = new Date();
+    const dtHoras = date.getHours();
+    const dtMinutos = date.getMinutes();
+    const dtSegundos = date.getSeconds();
+    return [dtHoras, dtMinutos, dtSegundos];
+}
+
+
+function getClockValues() {
+    const hours = document.querySelector("#clock-hours");
+    const minutes = document.querySelector("#clock-minutes");
+    const seconds = document.querySelector("#clock-seconds");
+    return [hours, minutes, seconds];
+}
+
+
+window.addEventListener("load", () => {
+    setInterval(createClock);
+})
