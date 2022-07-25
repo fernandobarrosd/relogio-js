2d02f33a168ea7621d79dc208cc3893003d3f456
= <=>
2022-07-25 18:52:01 -0300

    relogio-js update 3

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-function setRelogioValue(element, value) {
-    if (value < 10) {
-        element.textContent = "0" + value;
-    }
-    else {
-        element.textContent = value;
-    }
-}
+const clock = new Vue({
+    el: "#app",
+    data: {
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+    },
+   methods: {
+    createDate() {
+        const date = new Date();
+        const dtHoras = date.getHours();
+        const dtMinutos = date.getMinutes();
+        const dtSegundos = date.getSeconds();
+        return [dtHoras, dtMinutos, dtSegundos];
+    },
+    
+   },
+   beforeCreate() {
+    setInterval(() => {
+        const date = this.createDate();
+        this.hours = date[0];
+        this.minutes = date[1];
+        this.seconds = date[2];
+        
+    })
+   },
 
-function createClock() {
-    const date = createDate();
-    const clockValues = getClockValues();
-    setRelogioValue(clockValues[0], date[0]);
-    setRelogioValue(clockValues[1], date[1]);
-    setRelogioValue(clockValues[2], date[2]);
-}
-
-function createDate() {
-    const date = new Date();
-    const dtHoras = date.getHours();
-    const dtMinutos = date.getMinutes();
-    const dtSegundos = date.getSeconds();
-    return [dtHoras, dtMinutos, dtSegundos];
-}
-
-
-function getClockValues() {
-    const hours = document.querySelector("#clock-hours");
-    const minutes = document.querySelector("#clock-minutes");
-    const seconds = document.querySelector("#clock-seconds");
-    return [hours, minutes, seconds];
-}
-
-
-window.addEventListener("load", () => {
-    setInterval(createClock);
+   
 })
